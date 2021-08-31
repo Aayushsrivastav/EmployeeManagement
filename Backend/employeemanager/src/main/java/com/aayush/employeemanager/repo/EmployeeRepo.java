823b4fa2b0f61f1b563cee7536f0775e1ebc9500
Aayushsrivastav <aayushs2797@gmail.com>
2021-09-01 00:40:53 +0530

    Added employee repository and service

---
+++ Backend/employeemanager/src/main/java/com/aayush/employeemanager/repo/EmployeeRepo.java
@@ -0,0 +1,10 @@
+package com.aayush.employeemanager.repo;
+
+import com.aayush.employeemanager.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepo extends JpaRepository<Employee, Long> {
+    Optional<Employee> findEmployeeById(Long id);
+}
